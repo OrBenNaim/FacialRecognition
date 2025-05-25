a653ba696e16866248582e869f55826898eea4ab
OrBenNaim <orbennaim123@gmail.com>
2025-05-25 15:30:35 +0300

    .

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+from src.constants import OPTIMIZED_IMG_SHAPE
 from src.utils import analyze_dataset_distribution
 
-if __name__ == '__ main __':
+if __name__ == '__main__':
     print("Siamese Network Implementation for One-shot Face Recognition")
 
     # Analyze dataset first
-    analyze_dataset_distribution()
+    train_dist, test_dist = analyze_dataset_distribution()
+
+    # Create and run the model
+    siamese_model = SiameseFaceRecognition(input_shape=OPTIMIZED_IMG_SHAPE)
+
+    # siamese_model.run_complete_experiment(DATA_PATH, TRAIN_FILE, TEST_FILE)
+
+    print("\nTo run the experiment, update the paths above and uncomment the execution lines!")
