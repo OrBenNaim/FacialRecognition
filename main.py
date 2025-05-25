757271c4d2be7f291ee2a0211edb5bf093f02dea
OrBenNaim <orbennaim123@gmail.com>
2025-05-25 14:00:58 +0300

    call to analyze_dataset_distribution()

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-
+from src.utils import analyze_dataset_distribution
 
 if __name__ == '__ main __':
-    print("\nHi")
+    print("Siamese Network Implementation for One-shot Face Recognition")
+
+    # Analyze dataset first
+    analyze_dataset_distribution()
