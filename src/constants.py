d70bcc9260419d648a4ebe49deecddcda32e02dc
OrBenNaim <orbennaim123@gmail.com>
2025-05-25 15:29:38 +0300

    OPTIMIZED_IMG_SHAPE was added

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,7 @@
-
 DATA_PATH = "../DATA/LFW-a"  # Path to LFW-a dataset
-TRAIN_FILE = "../DATA/pairsDevTrain.txt"  # Path to train.txt
-TEST_FILE = "../DATA/pairsDevTest.txt"  # Path to test.txt
+TRAIN_FILE = './DATA/pairsDevTrain.txt'  # Path to train.txt
+TEST_FILE = './DATA/pairsDevTest.txt'  # Path to test.txt
+
+# Each image was a gray scale image with size of 250x250
+# Optimized image size is 128x128 pixels
+OPTIMIZED_IMG_SHAPE= (128, 128, 1)
