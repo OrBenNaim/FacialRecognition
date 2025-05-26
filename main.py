8e307df7af6c7a75b4f922897fdd0d125c3d1fe1
Or Ben Naim <orbennaim123@gmail.com>
2025-05-26 10:16:03 +0300

    Verify load_lfw_dataset()

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from src.constants import OPTIMIZED_IMG_SHAPE
+from src.constants import OPTIMIZED_IMG_SHAPE, DATA_PATH, TRAIN_FILE, TEST_FILE, VALIDATION_SPLIT
+from src.face_recognition_model import SiameseFaceRecognition
 from src.utils import analyze_dataset_distribution
 
 if __name__ == '__main__':
@@ -9,7 +10,8 @@ if __name__ == '__main__':
 
     # Create and run the model
     siamese_model = SiameseFaceRecognition(input_shape=OPTIMIZED_IMG_SHAPE)
-
+    train_person_images, test_person_images = siamese_model.load_lfw_dataset(DATA_PATH, TRAIN_FILE,
+                                                                             TEST_FILE, VALIDATION_SPLIT)
     # siamese_model.run_complete_experiment(DATA_PATH, TRAIN_FILE, TEST_FILE)
 
     print("\nTo run the experiment, update the paths above and uncomment the execution lines!")
