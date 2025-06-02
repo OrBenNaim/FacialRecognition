c81677c5c0bbd83076662cfb7b2f1b3563d3372e
Or Ben Naim <orbennaim123@gmail.com>
2025-06-02 22:35:35 +0300

    Use different experiments automatically

--- main.py
+++ main.py
@@ -5,14 +5,30 @@ from src.face_recognition_model import SiameseFaceRecognition
 if __name__ == '__main__':
     print("Siamese Network Implementation for One-shot Face Recognition")
 
-    # Create a model pipeline
-    siamese_model = SiameseFaceRecognition(input_shape=OPTIMIZED_IMG_SHAPE)
+    # Run multiple experiments
+    experiments = [
+        {"lr": 6e-5, "batch_size": 32, "epochs": 10},
+        {"lr": 3e-5, "batch_size": 64, "epochs": 20},
+    ]
 
-    # Load and prepare Train dataset
-    siamese_model.load_lfw_dataset(DATA_FOLDER_PATH, TRAIN_FILE_PATH, VALIDATION_SPLIT)
+    for exp in experiments:
 
-    # Analyze dataset first
-    siamese_model.analyze_train_val_dataset_distribution()
+        # Update constants
+        LEARNING_RATE = exp["lr"]
+        BATCH_SIZE = exp["batch_size"]
+        EPOCHS = exp["epochs"]
 
-    # Run the complete experiment
-    siamese_model.run_complete_experiment(f"lr{LEARNING_RATE}_bs{BATCH_SIZE}_epochs{EPOCHS}")
+        # Create a model pipeline
+        siamese_model = SiameseFaceRecognition(input_shape=OPTIMIZED_IMG_SHAPE,
+                                               experiment_name=f"lr{LEARNING_RATE}_bs{BATCH_SIZE}_epochs{EPOCHS}")
+
+        # Load and prepare Train dataset
+        siamese_model.load_lfw_dataset(DATA_FOLDER_PATH, TRAIN_FILE_PATH, VALIDATION_SPLIT)
+
+        # Analyze dataset first
+        siamese_model.analyze_train_val_dataset_distribution()
+
+        # Run experiment
+        siamese_model.run_complete_experiment()
+
+        print(f"✅ Completed {siamese_model.experiment_name}")
