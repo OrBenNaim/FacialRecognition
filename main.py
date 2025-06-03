40f7292afd870fa5ad097f63d05b87236d9c03af
OrBenNaim <orbennaim123@gmail.com>
2025-06-03 16:09:44 +0300

    .

--- main.py
+++ main.py
@@ -1,34 +1,30 @@
-from src.constants import OPTIMIZED_IMG_SHAPE, DATA_FOLDER_PATH, TRAIN_FILE_PATH, VALIDATION_SPLIT, LEARNING_RATE, \
-    BATCH_SIZE, EPOCHS
-from src.face_recognition_model import SiameseFaceRecognition
+from src.constants import OPTIMIZED_IMG_SHAPE, DATA_FOLDER_PATH, TRAIN_FILE_PATH, VALIDATION_SPLIT
+from src.face_recognition_model import FaceRecognition
 
 if __name__ == '__main__':
     print("Siamese Network Implementation for One-shot Face Recognition")
 
     # Run multiple experiments
     experiments = [
-        {"lr": 6e-5, "batch_size": 32, "epochs": 10},
-        {"lr": 3e-5, "batch_size": 64, "epochs": 20},
+        {"lr": 6e-5, "batch_size": 32, "epochs": 50},
+        {"lr": 3e-4, "batch_size": 32, "epochs": 50},
     ]
 
     for exp in experiments:
 
         # Update constants
-        LEARNING_RATE = exp["lr"]
-        BATCH_SIZE = exp["batch_size"]
-        EPOCHS = exp["epochs"]
+        learning_rate= exp["lr"]
+        batch_size = exp["batch_size"]
+        epochs = exp["epochs"]
 
         # Create a model pipeline
-        siamese_model = SiameseFaceRecognition(input_shape=OPTIMIZED_IMG_SHAPE,
-                                               experiment_name=f"lr{LEARNING_RATE}_bs{BATCH_SIZE}_epochs{EPOCHS}")
+        model = FaceRecognition(input_shape=OPTIMIZED_IMG_SHAPE, learning_rate=learning_rate,
+                                               batch_size=batch_size, epochs=epochs)
 
         # Load and prepare Train dataset
-        siamese_model.load_lfw_dataset(DATA_FOLDER_PATH, TRAIN_FILE_PATH, VALIDATION_SPLIT)
-
-        # Analyze dataset first
-        siamese_model.log_dataset_analysis_to_tensorboard()
+        model.load_lfw_dataset(DATA_FOLDER_PATH, TRAIN_FILE_PATH, VALIDATION_SPLIT)
 
         # Run experiment
-        siamese_model.run_complete_experiment()
+        model.run_complete_experiment(use_improved_arch=True)
 
-        print(f"✅ Completed {siamese_model.experiment_name}")
+        print(f"✅ Completed {model.experiment_name}")
