0d5b9ae8e79fd8fce62c4ba33a025db1babd444a
OrBenNaim <orbennaim123@gmail.com>
2025-06-08 14:17:24 +0300

    run_multiple_experiments() added

--- src/utils.py
+++ src/utils.py
@@ -15,6 +15,8 @@ from src.constants import (
     PIXEL_MIN_VALUE,
     PIXEL_MAX_VALUE
 )
+from src.face_recognition_model import FaceRecognition
+
 
 def plot_distribution_charts(train_val_dist: dict, save_dir: str = SAVE_IMG_DIR_PATH) -> None:
     """
@@ -119,3 +121,42 @@ def apply_simple_augmentation(image_array):
         image_array = np.clip(image_array + noise, PIXEL_MIN_VALUE, PIXEL_MAX_VALUE)
 
     return image_array
+
+def run_multiple_experiments(model: FaceRecognition):
+    # Run multiple experiments
+        experiments = [
+            # Enhanced base network with BatchNorm, Dropout, and smaller kernels
+            {"architecture_type": "improved", "lr": 6e-5, "batch_size": 32, "epochs": 50},
+
+            # Base convolutional network for feature extraction in Siamese architecture
+            {"architecture_type": "base", "lr": 6e-5, "batch_size": 32, "epochs": 50},
+        ]
+
+        for exp in experiments:
+            model.reset_exp_attr()
+
+            architecture_type = exp["architecture_type"]
+            learning_rate = exp["lr"]
+            batch_size = exp["batch_size"]
+            epochs = exp["epochs"]
+
+            exp_name = f"lr{learning_rate}_bs{batch_size}_epochs{epochs}"
+
+            use_improved_arch = None
+
+            if architecture_type == "base":
+                use_improved_arch = False
+                exp_name += "_base_arch"
+
+            elif architecture_type == "improved":
+                use_improved_arch = True
+                exp_name += "_improved_arch"    # Enhanced base network with BatchNorm, Dropout, and smaller kernels
+
+            elif architecture_type is None:
+                raise ValueError("Please specify the architecture type")
+
+            # Run experiment
+            model.run_complete_experiment(learning_rate=learning_rate,batch_size=batch_size,epochs=epochs,
+                                          use_improved_arch=use_improved_arch, exp_name=exp_name)
+
+            print(f"\n✅ Completed {model.experiment_name}")
