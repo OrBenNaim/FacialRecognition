27c4a87f17dce3a9cacfbfed9bd9f463904922f3
OrBenNaim <orbennaim123@gmail.com>
2025-06-08 15:57:07 +0300

    Need to write a clean pipline but model run on test dataset

--- main.py
+++ main.py
@@ -8,53 +8,6 @@ from src.constants import OPTIMIZED_IMG_SHAPE, DATA_FOLDER_PATH, TRAIN_FILE_PATH
 from src.face_recognition_model import FaceRecognition
 
 if __name__ == '__main__':
-    # print("\nSiamese Network Implementation for One-shot Face Recognition\n")
-    #
-    # # Create a model pipeline
-    # model = FaceRecognition(input_shape=OPTIMIZED_IMG_SHAPE)
-    #
-    # # Load and prepare Train dataset
-    # model.load_lfw_dataset(DATA_FOLDER_PATH, TRAIN_FILE_PATH, VALIDATION_SPLIT)
-    #
-    # model.tensorboard_log_dir = os.path.join("tensorboard_logs", "dataset_analysis")
-    # os.makedirs(model.tensorboard_log_dir, exist_ok=True)
-    # model.writer = SummaryWriter(log_dir=model.tensorboard_log_dir)
-    #
-    # # Log dataset analysis
-    # print("\n📊 Analyzing dataset...")
-    # model.log_dataset_analysis_to_tensorboard()
-
-    #run_multiple_experiments(model=model)
-
-    #============ Decided to move on with 'Base with Aug network' ============
-
-    # Run chosen experiment
-    # experiment = {"architecture_type": "base", "lr": 6e-5, "batch_size": 32, "epochs": 50}
-    #
-    # model.reset_exp_attr()
-    #
-    # architecture_type = experiment["architecture_type"]
-    # learning_rate = experiment["lr"]
-    # batch_size = experiment["batch_size"]
-    # epochs = experiment["epochs"]
-    #
-    # exp_name = f"lr{learning_rate}_bs{batch_size}_epochs{epochs}_BCE"
-    #
-    # use_improved_arch = None
-    #
-    # if architecture_type == "base":
-    #     use_improved_arch = False
-    #     exp_name += "_base_arch"
-    #
-    # else:
-    #     raise ValueError("Architecture type must be 'base' for this experiment. Please try again.")
-    #
-    # # Run experiment
-    # model.run_complete_experiment(learning_rate=learning_rate, batch_size=batch_size, epochs=epochs,
-    #                               use_improved_arch=use_improved_arch, exp_name=exp_name)
-    #
-    # print(f"\n✅ Completed {model.experiment_name}")
-
     #========= Run Model on Test Dataset =========
     print("🧪 Starting Test Phase...")
 
